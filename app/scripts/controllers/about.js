c312a5dd34ee1712e72c7ae387ad478d8460da2a
lenonrf@gmail.com <lenonrf@gmail.com>
2015-03-22 10:16:21 -0300

    Restruturação do projeto para se adequar a nova arquitetura

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gosteiclubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gosteiclubApp
+ */
+angular.module('gosteiclubApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gosteiclubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gosteiclubApp
+ */
+angular.module('gosteiclubApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
