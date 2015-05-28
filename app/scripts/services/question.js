274093b129b5f391a8b3573e18139f23da33fb5f
lenonrf@gmail.com <lenonrf@gmail.com>
2015-05-27 21:44:22 -0300

    cliente seu lima

--- app/scripts/services/question.js
+++ app/scripts/services/question.js
@@ -20,6 +20,28 @@ angular.module('gosteiclubApp')
         idQuestion : 1,
         score : 560,
         iframeHeight: 1250,
+        image: 'claro.jpeg',
+        title: 'Em época de crise é bom rever os plano de TV a cabo, que tal pagar apenas 49,90?',
+        description: '90 canais com 2 equipamentos!',
+        urlAnswer: ''
+
+      };
+
+      var question_2 = {
+        idQuestion : 1,
+        score : 560,
+        iframeHeight: 1250,
+        image: 'udbc.jpeg',
+        title: 'Ganhe Grátis um Tablet',
+        description: 'Nos cursos do Portal Educação você ganha um tablet',
+        urlAnswer: 'http://www.portaleducacao.com.br/adnet/lppos/?'
+
+      };
+
+      var question_3 = {
+        idQuestion : 1,
+        score : 560,
+        iframeHeight: 1250,
         image: 'english.jpeg',
         title: 'Como está o seu Inglês? Te damos 14 dias Grátis na EnglishTown.',
         description: 'inscreva-se e utilize 14 dias sem pagar nada.',
@@ -27,37 +49,18 @@ angular.module('gosteiclubApp')
 
       };
 
-     var question_2 = {
+     var question_4 = {
         idQuestion : 2,
         score : 100,
         image: 'seulima.jpeg',
         title: 'Está no ar a promoção Encontro Premiado, ganhe até 350 mil reais.',
-        description: 'é só clicar e participar, é sem pagar nada!',
+        description: 'É só clicar e participar, é sem pagar nada!',
         urlAnswer: 'http://encontropremiado.com.br/cadastro/criar/?mktcode=X4EL01&transaction_id=&nome=&sobrenome=&email='
 
       };
 
-      /* var question_3 = {
-        idQuestion : 3,
-        score : 200,
-        image: 'greenpeace.jpg',
-        title: 'Ajude o Greenpeace',
-        description: 'Deseja receber um super desconto na nossa mensalidade?',
-        urlAnswer: 'https://junte-se-ao-greenpeace.org.br/2014/?appeal=12446&utm_source=referral&utm_medium=partner'
 
-      };
-
-      var question_4 = {
-        idQuestion : 4,
-        score : 130,
-        image: 'chevrolet.png',
-        title: 'Ganhe um carro chevolet',
-        description: 'Mensalmente, o dinheiro é retirado da conta para compor um ou mais bens. A pessoa pode ser contemplada por lance (oferecer maior percentual em relação ao valor do bem) ou sorteio.',
-        urlAnswer: 'http://compare-ja.com/chevrolet/'
-
-      };*/
-
-      return [question_1, question_2];
+      return [question_1, question_2, question_3, question_4];
 
       //return [question_1, question_2, question_3, question_4];
     };
