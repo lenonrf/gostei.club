2f4c99eb6026280ad2893fe441acaaec9e5d5d8f
lenonrf@gmail.com <lenonrf@gmail.com>
2016-07-05 19:08:32 -0300

    conectai

--- app/scripts/controllers/obrigado.js
+++ app/scripts/controllers/obrigado.js
@@ -2,7 +2,7 @@
 
 
 angular.module('gosteiclubApp')
-  .controller('ObrigadoCtrl', function ($scope, $location, $rootScope, Menu) {
+  .controller('ObrigadoCtrl', function ($scope, $location, $http, $rootScope, Menu) {
 
     Menu.setMenu('HomeCtrl');
     $rootScope.showFooter = false;
@@ -13,6 +13,13 @@ angular.module('gosteiclubApp')
     if($location.search().utm_source){
       if ($location.search().utm_source.toLowerCase() === 'conectai'){
         $scope.isConectai = true;
+
+        $http.post('/api/conectai').success(function(dataResult){
+          console.log('SUCCESS', dataResult);
+        }).error(function(){
+          console.log('ERROR');
+        });
+
       }
     }
 
