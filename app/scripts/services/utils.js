61a580477ecd40942e8f32339646c76104c76637
lenonrf@gmail.com <lenonrf@gmail.com>
2016-09-14 00:24:18 -0300

    allin correcao

--- app/scripts/services/utils.js
+++ app/scripts/services/utils.js
@@ -56,7 +56,12 @@ angular.module('gosteiclubApp')
     this.getDateAllin = function(){
 
       var date = new Date();
-      return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+
+      var year = date.getFullYear();
+      var month = ( (date.getMonth()+1) < 10) ? ("0" + (date.getMonth()+1)) : (date.getMonth()+1);
+      var day = date.getDate();
+      
+      return year+'-'+month+'-'+day;
     };
 
 
